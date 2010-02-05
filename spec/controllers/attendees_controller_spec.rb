29e8f831d9d9b35aabe4a56e0cdd0e4e82400de5
interu <beautybeast07@gmail.com>
2010-02-05 19:03:30 +0900

    added spec

---
+++ spec/controllers/attendees_controller_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+describe AttendeesController do
+
+  #Delete this example and add some real ones
+  it "should use AttendeesController" do
+    controller.should be_an_instance_of(AttendeesController)
+  end
+
+end
