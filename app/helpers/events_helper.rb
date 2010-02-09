80ea7fb932651f30917fbb33468c366929c3bfc5
interu <beautybeast07@gmail.com>
2010-02-09 17:44:29 +0900

    modified design on event

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -2,4 +2,8 @@ module EventsHelper
   def attendee_icon attendee
     attendee.status ? icon_tag('lightbulb', :title => _('Attendance')) : icon_tag('cross', :title => _('Absence'))
   end
+
+  def event_type_icon event
+    icon_tag('key', :title => _('Need invitation of the Administrator')) if event.publication_type == 'protected'
+  end
 end
