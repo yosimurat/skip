1ee0aeaa04b37b38eda38dd6735e864763df1dfa
maedana <maeda.na@gmail.com>
2010-02-23 17:18:54 +0900

    fix migrate error

--- db/migrate/20091216092645_add_notice_type_to_user_readings.rb
+++ db/migrate/20091216092645_add_notice_type_to_user_readings.rb
@@ -2,7 +2,7 @@ class AddNoticeTypeToUserReadings < ActiveRecord::Migration
   def self.up
     add_column :user_readings, :notice_type, :string
     add_index :user_readings, :notice_type
-    notice_entries_ids = BoardEntry.notice.map(&:id)
+    notice_entries_ids = BoardEntry.scoped(:conditions => ['board_entries.aim_type = \'notice\'']).map(&:id) 
     UserReading.all.each do |ur|
       ur.update_attributes(:notice_type => 'notice') if notice_entries_ids.include?(ur.board_entry_id)
     end
