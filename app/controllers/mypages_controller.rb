b185627b340fa483851b877f36da53a4934472b9
maedana <maeda.na@gmail.com>
2010-04-23 10:12:59 +0900

    グループカテゴリが検索条件に反映されていなかったので(グループカテゴリに属するグループのIDのみ対象となるように修正)

--- app/controllers/mypages_controller.rb
+++ app/controllers/mypages_controller.rb
@@ -277,7 +277,7 @@ class MypagesController < ApplicationController
   def find_recent_bbs_as_locals code, options = {}
     category = current_tenant.group_categories.find_by_code(code)
     id_name = category.code.downcase
-    pages = BoardEntry.from_recents.accessible(current_user).entry_type_is(BoardEntry::GROUP_BBS).timeline.scoped(:include => [ :user, :state ]).order_new.paginate(:page => target_page(id_name), :per_page => options[:per_page])
+    pages = BoardEntry.from_recents.accessible(current_user).owner_type_is('Group').owner_id_is(category.groups.map(&:id)).timeline.scoped(:include => [ :user, :state ]).order_new.paginate(:page => target_page(id_name), :per_page => options[:per_page])
 
     locals = {
       :id_name => id_name,
